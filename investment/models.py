12ae3fea8792aabee034c9a1ef49394b23e1dafe
Dosu333 <vladamir1865@gmail.com>
2023-02-13 14:42:49 +0100

    available investments model

--- investment/models.py
+++ investment/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+import uuid
 
-# Create your models here.
+class AvailableInvestment(models.Model):
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    name = models.CharField(max_length=225)
+    interest_rate = models.IntegerField()
+    investment = models.IntegerField()
+    max_earning = models.IntegerField()
+    investment_duration = models.IntegerField()
+    investors = models.ManyToManyField(get_user_model())
+
+    def __str__ (self):
+        return self.name
