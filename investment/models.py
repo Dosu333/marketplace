8c5709135b6d5bae3dd5f0e6c22f834b93aa6d6d
Dosu333 <vladamir1865@gmail.com>
2023-07-16 20:59:47 +0000

    add image to availabe investments

--- investment/models.py
+++ investment/models.py
@@ -8,6 +8,7 @@ class AvailableInvestment(models.Model):
     interest_rate = models.IntegerField()
     investment = models.IntegerField()
     max_earning = models.IntegerField()
+    image = models.FileField(upload_to='investmemt/', null=True, blank=True)
     investment_duration = models.IntegerField()
     investors = models.ManyToManyField(get_user_model())
 
